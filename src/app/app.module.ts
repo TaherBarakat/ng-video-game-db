a18da3ccff33d87b4e12ab9aa4667eef72f35a1b
Taher Barakat <taher.barakat12223@gmail.com>
2024-04-17 18:18:10 +0300

    create http service and its interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { GaugeModule } from 'angular-gauge';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -16,6 +16,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeComponent } from './components/home/home.component';
+import { HttpErrorsInterceptor } from './interseptors/http-errors.interseptor';
+import { HttpHeadersInterceptor } from './interseptors/http-headers.interseptors';
 @NgModule({
   declarations: [AppComponent, SearchBarComponent, HomeComponent],
   imports: [
@@ -30,7 +32,18 @@ import { HomeComponent } from './components/home/home.component';
     MatFormFieldModule,
     MatSelectModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
